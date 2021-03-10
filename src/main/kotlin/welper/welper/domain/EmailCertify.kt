6a50eb6fdb0d6e839730bc7f53730d0a782956c5
jun <toinin@naver.com>
2021-03-10 15:51:55 +0900

    Add approval email service

--- src/main/kotlin/welper/welper/domain/EmailCertify.kt
+++ src/main/kotlin/welper/welper/domain/EmailCertify.kt
@@ -4,15 +4,21 @@ import javax.persistence.*;
 
 @Entity
 class EmailCertify(
-
         @Id
         @Column(name = "email")
         val email: String,
-
         @Column(name = "authCode")
         val authCode: String,
+        certified: Boolean
+) {
 
-        @Column(name="certified")
-        val certified:Boolean,
 
-)
+    @Column(name = "certified", nullable = false, length = 555)
+    var certified = certified
+        private set
+
+    fun isCertified(certified: Boolean) {
+        this.certified = true
+    }
+
+}
--- src/main/kotlin/welper/welper/service/EmailService.kt
+++ src/main/kotlin/welper/welper/service/EmailService.kt
@@ -12,42 +12,49 @@ import java.util.*
 
 @Service
 
-class EmailService (
-        private val javaMailSender:JavaMailSender,
+class EmailService(
+        private val javaMailSender: JavaMailSender,
         private val emailCertifyRepository: EmailCertifyRepository,
-){
+) {
     private val encryptionAlgorithm = "SHA-512"
     private val characterEncoding = Charset.forName("UTF-8")
 
-    fun send(email:String){
-        val randomCode :String = getRandomString(10)
+    fun send(email: String) {
+        val randomCode: String = getRandomString(10)
 
         emailCertifyRepository.save(
-            EmailCertify(
-                    email=email,
-                    authCode = encodingPassword(randomCode),
-                    certified = false,
-            )
+                EmailCertify(
+                        email = email,
+                        authCode = encodingPassword(randomCode),
+                        certified = false,
+                )
         )
+        val message = SimpleMailMessage()
+        message.setFrom("a")
+        message.setTo(email)
+        message.setSubject("Welper 인증코드")
+        println(randomCode)
+        message.setText("인증 코드: $randomCode")
+        javaMailSender.send(message)
 
-                val message = SimpleMailMessage()
-                message.setFrom("a")
-                message.setTo(email)
-                message.setSubject("Welper 인증코드")
-                message.setText("인증 코드: $randomCode")
-                javaMailSender.send(message)
-
-
-    }
-    fun approvalMail(authCode:String,email:String) {
 
     }
+
+    fun approvalMail(authCode: String, email: String) {
+        val emailCertify: EmailCertify = emailCertifyRepository.findByEmailAndAuthCode(email = email, authCode = encodingPassword(authCode))
+                ?: throw Exception()
+        emailCertify.isCertified(emailCertify.certified)
+        emailCertifyRepository.save(emailCertify)
+    }
+
+
     private fun encodingPassword(originalPassword: String): String {
         val messageDigest = MessageDigest.getInstance(encryptionAlgorithm)
         messageDigest.update(originalPassword.toByteArray(characterEncoding))
         return String.format("%0128x", BigInteger(1, messageDigest.digest()))
     }
-    private fun getRandomString(length: Int) : String {
+
+    private fun getRandomString(length: Int): String {
         val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9')
         return (1..length)
                 .map { charset.random() }
