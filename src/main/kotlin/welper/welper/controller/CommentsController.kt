5a9523df49d84ddafc14f2e98409eb5bce1613c2
jun <toinin@naver.com>
2021-03-12 13:11:46 +0900

    Add CommentsController

--- src/main/kotlin/welper/welper/controller/CommentsController.kt
+++ src/main/kotlin/welper/welper/controller/CommentsController.kt
@@ -1,9 +1,20 @@
 package welper.welper.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/comments")
 class CommentsController {
+    @PostMapping("/{postId}/{commentsId}")
+    fun commentsWrite(@PathVariable postId:Int, @PathVariable commentsId:Int){
+
+    }
+    @GetMapping("/{postId}")
+    fun commentsRead(@PathVariable postId: Int){
+
+    }
+    @DeleteMapping("/{postId}/{commentsId}")
+    fun commentsDelete(@PathVariable postId: Int, @PathVariable commentsId:Int){
+    }
+
 }
