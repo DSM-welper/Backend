659e73832faed3e6f5d00ca0845a3c8315a157b2
jun <toinin@naver.com>
2021-03-13 15:54:26 +0900

    Add CommentsChild service

--- src/main/kotlin/welper/welper/service/CommentsService.kt
+++ src/main/kotlin/welper/welper/service/CommentsService.kt
@@ -42,7 +42,7 @@ class CommentsService(
         commentRepository.save(
                 Comments(
                         parents = commentsId,
-                        depts = 0,
+                        depts = comments.depts+1,
                         sequence = comments.sequence + i3,
                         comments = content,
                         postId = post.id,
@@ -54,16 +54,14 @@ class CommentsService(
         val email: String = jwtService.getEmail(token)
         val user: User = userRepository.findByIdOrNull(email) ?: throw UserNotFoundException(email)
         val post: Post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException(email, postId)
-        val comments: List<Comments?> = commentRepository.findAllByParentsAndDepts(0, 0)
-        var num: Int = 1
-        for (i in 1..comments.size)
-            num++
+        val comments: List<Comments?> = commentRepository.findAll()
+
 
         commentRepository.save(
                 Comments(
                         parents = 0,
                         depts = 0,
-                        sequence = num,
+                        sequence = comments.size+1,
                         comments = content,
                         postId = post.id,
                 )
