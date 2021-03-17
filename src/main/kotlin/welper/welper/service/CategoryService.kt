588b1e4b55741928ae239a9f5ee80db564379e5b
jun <toinin@naver.com>
2021-03-17 15:17:37 +0900

    Add Value deduplication

--- src/main/kotlin/welper/welper/service/CategoryService.kt
+++ src/main/kotlin/welper/welper/service/CategoryService.kt
@@ -36,7 +36,6 @@ class CategoryService(
         }
         val categoryList: MutableList<OpenApICategory> =
                 openApiCategoryRepository.findByCategoryName3(list)
-
         val servList: MutableList<CategoryListPostResponse.ServList> = mutableListOf()
 
         categoryList.forEach {
@@ -57,7 +56,6 @@ class CategoryService(
         val set: List<CategoryListPostResponse.ServList> = servList.groupBy { it.servId }
                 .filter { it.value.size == categoryNameList.count() }.flatMap { it.value }
 
-
         return CategoryListPostResponse(
                 servList = set
         )
@@ -76,10 +74,10 @@ class CategoryService(
         val eElement = nNode as Element
         return CategoryDetailResponse(
                 alwServCn = getTagValue("alwServCn", eElement),
-                applmetList = createApplmetList(doc),
-                basfrmList = createBasfrmList(doc),
-                baslawList = createBaslawList(doc),
-                inqplCtadrList = createInqplCtarList(doc),
+                applmetList = createDetailList(doc, "applmetList"),
+                basfrmList = createDetailList(doc, "basfrmList"),
+                baslawList = createDetailList(doc, "baslawList"),
+                inqplCtadrList = createDetailList(doc, "inqplCtadrList"),
                 servDgst = getTagValue("servDgst", eElement),
                 servId = getTagValue("servId", eElement),
                 servNm = getTagValue("servNm", eElement),
@@ -88,41 +86,42 @@ class CategoryService(
         )
     }
 
-    private fun categoryURL(
-            num: Int,
-            list: MutableList<Document>, searchWrd: String,
-    )
-            : MutableList<Document> {
-        println("시작$num")
-        var num2 = num
-        val urlstr = "http://www.bokjiro.go.kr/openapi/rest/gvmtWelSvc" +
-                "?crtiKey=" +
-                "keTuCooJ8R9Ao5LERVj48XiH87g5hLr3teCu06S8KTfHxSwtGkz0nAS%2BYS8v35JrIJ%2FxYDe3%2BtshuX2%2B2EZg3w%3D%3D" +
-                "&callTp=L" +
-                "&pageNo=$num2" +
-                "&numOfRows=100$searchWrd"
-        val dbFactoty: DocumentBuilderFactory = DocumentBuilderFactory.newInstance();
-        val dBuilder: DocumentBuilder = dbFactoty.newDocumentBuilder();
-        val doc: Document = dBuilder.parse(urlstr)
-        println(doc.getElementsByTagName("servList").length)
-        if (doc.getElementsByTagName("servList").length == 100) {
-            num2++
-            categoryURL(num2, list, searchWrd)
+    fun categorySearch(content: String): CategoryListPostResponse {
+        val list = openApiPostRepository.findAll()
+        val servList: MutableList<CategoryListPostResponse.ServList> = mutableListOf()
+        list.filter {
+            it.servDgst.contains(content)
+        }.forEach {
+            servList.add(
+                    CategoryListPostResponse.ServList(
+                            inqNum = it.inqNum,
+                            jurMnofNm = it.jurMnofNm,
+                            jurOrgNm = it.jurOrgNm,
+                            servDgst = it.servDgst,
+                            servDtlLink = it.servDtlLink,
+                            servId = it.servId,
+                            servNm = it.servNm,
+                            svcfrstRegTs = it.svcfrstRegTs,
+                    )
+            )
         }
-        list.add(doc)
 
-        return list
+        return CategoryListPostResponse(
+                servList = servList
+        )
     }
 
-    private fun createBaslawList(doc: Document): List<CategoryDetailResponse.BaslawList> {
-        val nList: NodeList = doc.getElementsByTagName("baslawList");
-        val list: ArrayList<CategoryDetailResponse.BaslawList> = ArrayList()
+
+    private fun createDetailList(doc: Document, targetName: String)
+            : List<CategoryDetailResponse.DetailList> {
+        val nList: NodeList = doc.getElementsByTagName(targetName)
+        val list: MutableList<CategoryDetailResponse.DetailList> = mutableListOf()
 
         println("size:${nList.length}")
         for (i in 0 until nList.length) {
             val nNode: Node = nList.item(i)
             val eElement = nNode as Element
-            val a = CategoryDetailResponse.BaslawList(
+            val a = CategoryDetailResponse.DetailList(
                     servSeCode = getTagValue("servSeCode", eElement),
                     servSeDetailNm = getTagValue("servSeDetailNm", eElement),
                     servSeDetailLink = getTagValue("servSeDetailNm", eElement)
@@ -132,98 +131,10 @@ class CategoryService(
         return list
     }
 
-    private fun createBasfrmList(doc: Document): List<CategoryDetailResponse.BasfrmList> {
-        val nList: NodeList = doc.getElementsByTagName("basfrmList");
-        val list: ArrayList<CategoryDetailResponse.BasfrmList> = ArrayList()
-        println("size:${nList.length}")
-
-        for (i in 0 until nList.length) {
-            val nNode: Node = nList.item(i)
-            val eElement = nNode as Element
-            val a = CategoryDetailResponse.BasfrmList(
-                    servSeCode = getTagValue("servSeCode", eElement),
-                    servSeDetailNm = getTagValue("servSeDetailNm", eElement),
-                    servSeDetailLink = getTagValue("servSeDetailLink", eElement)
-            )
-            list.add(a)
-        }
-        return list
-    }
-
-    private fun createInqplCtarList(doc: Document): List<CategoryDetailResponse.InqplCtadrList> {
-        val nList: NodeList = doc.getElementsByTagName("inqplCtadrList");
-
-        val list: ArrayList<CategoryDetailResponse.InqplCtadrList> = ArrayList()
-        for (i in 0 until nList.length) {
-            val nNode: Node = nList.item(i)
-            val eElement = nNode as Element
-            val a = CategoryDetailResponse.InqplCtadrList(
-                    servSeCode = getTagValue("servSeCode", eElement),
-                    servSeDetailNm = getTagValue("servSeDetailNm", eElement),
-                    servSeDetailLink = getTagValue("servSeDetailLink", eElement)
-            )
-            list.add(a)
-        }
-        return list
-    }
-
-    private fun createApplmetList(doc: Document): List<CategoryDetailResponse.ApplmetList> {
-        val nList: NodeList = doc.getElementsByTagName("applmetList");
-        val list: ArrayList<CategoryDetailResponse.ApplmetList> = ArrayList()
-        for (i in 0 until nList.length) {
-            val nNode: Node = nList.item(i)
-            val eElement = nNode as Element
-            val a = CategoryDetailResponse.ApplmetList(
-                    servSeCode = getTagValue("servSeCode", eElement),
-                    servSeDetailNm = getTagValue("servSeDetailNm", eElement),
-                    servSeDetailLink = getTagValue("servSeDetailLink", eElement)
-            )
-            list.add(a)
-        }
-        return list
-    }
-
-    private fun createServList(doc: Document): List<CategoryListPostResponse.ServList> {
-
-        val nList: NodeList = doc.getElementsByTagName("servList");
-        println("size:${nList.length}")
-
-        val list: ArrayList<CategoryListPostResponse.ServList> = ArrayList()
-        for (i in 0 until nList.length) {
-            val nNode: Node = nList.item(i)
-            val eElement = nNode as Element
-            val a = CategoryListPostResponse.ServList(
-                    inqNum = getTagValue("inqNum", eElement),
-                    jurOrgNm = getTagValue("jurOrgNm", eElement),
-                    servDgst = getTagValue("servDgst", eElement),
-                    servDtlLink = getTagValue("servDtlLink", eElement),
-                    servId = getTagValue("servId", eElement),
-                    servNm = getTagValue("servNm", eElement),
-                    svcfrstRegTs = getTagValue("svcfrstRegTs", eElement),
-                    jurMnofNm = getTagValue("jurMnofNm", eElement),
-            )
-            list.add(a)
-        }
-        return list
-    }
-
-
     private fun getTagValue(tag: String, eElement: Element): String? {
         val nlList: NodeList = eElement.getElementsByTagName(tag).item(0).childNodes ?: return "a"
         val nValue: Node = nlList.item(0) ?: return "a"
         return nValue.nodeValue
     }
 
-
-    fun categorySearch(content: String): CategoryListPostResponse {
-        val list: MutableList<Document> = mutableListOf()
-        val docList: MutableList<Document> = categoryURL(1, list, content)
-        val servList: MutableList<CategoryListPostResponse.ServList> = mutableListOf()
-        docList.forEach {
-            servList.addAll(createServList(it))
-        }
-        return CategoryListPostResponse(
-                servList = servList
-        )
-    }
 }
