33974825c4ba1dd3eb40993f1dbf102497e43f89
jun <toinin@naver.com>
2021-03-20 16:51:41 +0900

    Fix postRead for comments

--- src/main/kotlin/welper/welper/service/PostService.kt
+++ src/main/kotlin/welper/welper/service/PostService.kt
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Service
 import welper.welper.controller.response.CategoryListPostResponse
 import welper.welper.controller.response.PostListResponse
 import welper.welper.controller.response.PostResponse
+import welper.welper.domain.Comments
 import welper.welper.domain.Post
 import welper.welper.domain.User
 import welper.welper.exception.PostListNotFoundException
 import welper.welper.exception.PostNotFoundException
 import welper.welper.exception.UserNotFoundException
+import welper.welper.repository.CommentsRepository
 import welper.welper.repository.PostRepository
 import welper.welper.repository.UserRepository
 import java.time.LocalDateTime
@@ -19,6 +21,7 @@ class PostService(
         val jwtService: JwtService,
         val userRepository: UserRepository,
         val postRepository: PostRepository,
+        val commentsRepository: CommentsRepository,
 ) {
     fun postCreate(token: String, title: String, content: String, category: String, createdAt: LocalDateTime) {
         val email: String = jwtService.getEmail(token)
@@ -46,6 +49,20 @@ class PostService(
         val email: String = jwtService.getEmail(token)
         val user: User = userRepository.findByIdOrNull(email) ?: throw UserNotFoundException(email)
         val post: Post = postRepository.findByIdOrNull(id) ?: throw PostNotFoundException(email, id)
+        val comments: List<Comments?> = commentsRepository.findAllByPostId(id)
+
+        val list: MutableList<PostResponse.CommentsResponse> = mutableListOf()
+        comments.map {
+            if (it != null)
+                list.add(PostResponse.CommentsResponse(
+                        id = it.id,
+                        parents = it.parents,
+                        depts = it.depts,
+                        comments = it.comments,
+                        postId = it.postId,
+                        writer = it.user.name,
+                ))
+        }
 
         return PostResponse(
                 title = post.title,
@@ -53,6 +70,7 @@ class PostService(
                 createdAt = post.createdAt,
                 category = post.category,
                 writer = user.name,
+                comment = list,
         )
     }
 
