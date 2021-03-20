ea2c25df9baeebda3dc20763898c7e7e996414d3
jun <toinin@naver.com>
2021-03-20 19:14:36 +0900

    Add objectConfiguration

---
+++ src/main/kotlin/welper/welper/configuration/ObjectMapperConfiguration.kt
@@ -0,0 +1,13 @@
+package welper.welper.configuration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfiguration {
+
+    @Bean
+    fun objectMapper(): ObjectMapper = jacksonObjectMapper().findAndRegisterModules()
+}
