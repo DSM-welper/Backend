2e2e348101419ce9a1549b14872cae7a5847e410
jun <toinin@naver.com>
2021-03-22 19:02:05 +0900

    Add validateToken mainController

--- src/main/kotlin/welper/welper/controller/MainController.kt
+++ src/main/kotlin/welper/welper/controller/MainController.kt
@@ -6,6 +6,7 @@ import welper.welper.controller.response.RandomCategoryResponse
 import welper.welper.domain.User
 import welper.welper.exception.UserNotFoundException
 import welper.welper.repository.UserRepository
+import welper.welper.service.AuthService
 import welper.welper.service.CategoryService
 import welper.welper.service.JwtService
 
@@ -13,11 +14,13 @@ import welper.welper.service.JwtService
 @RequestMapping("/main")
 class MainController(
         private val categoryService: CategoryService,
-        ) {
+        private val authService: AuthService,
+) {
     @GetMapping
     fun randomCategory(@RequestHeader("Authorization") token: String?):
             RandomCategoryResponse {
         token ?: return categoryService.randomCategory()
+        authService.validateToken(token)
         return categoryService.userCategory(
                 token = token
         )
