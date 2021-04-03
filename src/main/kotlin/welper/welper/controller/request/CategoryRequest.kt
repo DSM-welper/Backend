297d1f4d9c361659e140957550170703d6f9a86b
jun <toinin@naver.com>
2021-04-04 00:19:18 +0900

    .

--- src/main/kotlin/welper/welper/controller/request/CategoryRequest.kt
+++ src/main/kotlin/welper/welper/controller/request/CategoryRequest.kt
@@ -2,7 +2,6 @@ package welper.welper.controller.request
 
 import welper.welper.domain.attribute.Category
 
-
 data class CategoryRequest(
         val numOfPage:Int,
         val categoryName:List<Category>
