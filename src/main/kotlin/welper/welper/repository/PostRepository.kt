93b09eb90249be8209e1124530049cd4e23094f9
jun <toinin@naver.com>
2021-04-10 17:58:25 +0900

    Fix Pageable

--- src/main/kotlin/welper/welper/repository/PostRepository.kt
+++ src/main/kotlin/welper/welper/repository/PostRepository.kt
@@ -1,13 +1,18 @@
 package welper.welper.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import welper.welper.domain.Post
 import welper.welper.domain.User
 import java.util.*
 
 interface PostRepository:JpaRepository<Post,Int>{
     fun findByIdAndUser(id: Int, user: User):Post?
-    fun findAllByCategory(category: String):List<Post?>
-    fun findAllByUser(user: User):List<Post?>
+    fun findAllByCategory(category: String,pageable: Pageable):Page<Post>
+    fun findAllByUser(user: User,pageable: Pageable):Page<Post>
     fun findPostById(id: Int): Post?
+    @Query("Select c from Post c where c.title like :content" )
+    fun findPostBySearch(content:String,pageable:Pageable):Page<Post>
 }
