ceb481693381b1046bea804cd21d6e7fa0a7edbb
jun <toinin@naver.com>
2021-04-13 22:37:51 +0900

    ADd jsonformat localdatetime

--- src/main/kotlin/welper/welper/controller/response/PostListResponse.kt
+++ src/main/kotlin/welper/welper/controller/response/PostListResponse.kt
@@ -1,5 +1,6 @@
 package welper.welper.controller.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class PostListResponse (
@@ -10,6 +11,7 @@ data class PostListResponse (
     class PostList(
         val id:Int,
         val title:String,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         val creatAt:LocalDateTime,
         val category:String,
         val writer :String,
