608e54594b3bdc6bfb75c6eba5745f579c02ffef
jun <toinin@naver.com>
2021-05-18 14:18:32 +0900

    FIx controller - showCategoryList

--- src/main/kotlin/welper/welper/controller/CategoryController.kt
+++ src/main/kotlin/welper/welper/controller/CategoryController.kt
@@ -25,7 +25,7 @@ class CategoryController(
     }
 
     @GetMapping
-    fun showCategoryList(@RequestHeader("Authorization") token: String, request: PageRequest): CategoryListPostResponse {
+    fun showCategoryList(@RequestHeader("Authorization") token: String?, request: PageRequest): CategoryListPostResponse {
         return categoryService.showCategoryList(request.numOfPage,token)
     }
 
