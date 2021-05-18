518977d1e8a009785294a1f81a258e68703ecb3c
jun <toinin@naver.com>
2021-05-18 11:15:00 +0900

    Add response - mypage

--- src/main/kotlin/welper/welper/controller/response/MyPageResponse.kt
+++ src/main/kotlin/welper/welper/controller/response/MyPageResponse.kt
@@ -1,13 +1,16 @@
 package welper.welper.controller.response
 
+import welper.welper.domain.OpenApiPost
 import welper.welper.domain.attribute.Gender
 import welper.welper.domain.attribute.Marry
 
-data class MyPageResponse (
-        val email:String,
-        val marry:Marry,
+data class MyPageResponse(
+        val email: String,
+        val marry: Marry,
         val gender: Gender,
-        val age:Int,
-        val name:String,
-        val disorder:Boolean,
-        )
+        val age: Int,
+        val name: String,
+        val disorder: Boolean,
+        val bookMark: List<OpenApiPost>,
+        val totalPage: Int,
+)
