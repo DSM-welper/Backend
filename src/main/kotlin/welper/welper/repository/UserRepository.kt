eb8fdd868805a48bc19cc1be854428bec170ae23
jun <toinin@naver.com>
2021-06-01 20:24:58 +0900

    FIx user -> email

--- src/main/kotlin/welper/welper/repository/UserRepository.kt
+++ src/main/kotlin/welper/welper/repository/UserRepository.kt
@@ -3,6 +3,8 @@ package welper.welper.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import welper.welper.domain.User
 
-interface UserRepository:JpaRepository<User,String>{
-        fun findByEmail(email: String):User?
+interface UserRepository : JpaRepository<User, String> {
+    fun findByEmail(email: String): User?
+    fun existsByEmail(email: String): Boolean
+
 }
