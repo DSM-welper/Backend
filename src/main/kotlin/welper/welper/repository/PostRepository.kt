17260ac99eb1deba4a08cac9708b745d469a2299
jun <toinin@naver.com>
2021-06-01 20:25:08 +0900

    FIx user -> email

--- src/main/kotlin/welper/welper/repository/PostRepository.kt
+++ src/main/kotlin/welper/welper/repository/PostRepository.kt
@@ -9,9 +9,9 @@ import welper.welper.domain.User
 import java.util.*
 
 interface PostRepository:JpaRepository<Post,Int>{
-    fun findByIdAndUser(id: Int, user: User):Post?
+    fun findByIdAndEmail(id: Int, email: String, ):Post?
     fun findAllByCategory(category: String,pageable: Pageable):Page<Post>
-    fun findAllByUser(user: User,pageable: Pageable):Page<Post>
+    fun findAllByEmail(email: String, pageable: Pageable):Page<Post>
     fun findPostById(id: Int): Post?
     @Query("Select c from Post c where c.title like %:content%" )
     fun findPostBySearch(content:String,pageable:Pageable):Page<Post>
